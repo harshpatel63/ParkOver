7f785207a457e185d743d9ea8b118512f0fbfbee
Arjun Gupta <arjungupta0817@gmail.com>
2023-01-07 02:57:20 +0530

    <ArjunGupta><added fragments,gradle Dependencies,xml and drawable files.>

--- app/src/main/java/com/example/parkover/MainActivity.kt
+++ app/src/main/java/com/example/parkover/MainActivity.kt
@@ -3,15 +3,42 @@ package com.example.parkover
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.parkover.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
+//    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomBar: AnimatedBottomBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        val view = binding.root
+//        setContentView(view)
+        setContentView(R.layout.activity_main)
+        bottomBar = findViewById(R.id.bottom_bar)
+        replaceFragment(MapFragment())
+        bottomBar.setOnTabSelectListener(object :AnimatedBottomBar.OnTabSelectListener{
+            override fun onTabSelected(
+                lastIndex: Int,
+                lastTab: AnimatedBottomBar.Tab?,
+                newIndex: Int,
+                newTab: AnimatedBottomBar.Tab
+            ) {
+                when(newIndex){
+                    0->replaceFragment(MapFragment())
+                    1->replaceFragment(ARFragment())
+                    else-> {
+
+                    }
+                }
+            }
+
+        })
+    }
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+            .commit()
+
     }
 }

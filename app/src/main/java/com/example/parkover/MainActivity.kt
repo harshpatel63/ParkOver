c0e8c95921cbf343a1ee28deb7f53ea0a266ee5b
Harsh <patelharsh0603@gmail.com>
2023-01-07 23:14:40 +0530

    rebase

--- app/src/main/java/com/example/parkover/MainActivity.kt
+++ app/src/main/java/com/example/parkover/MainActivity.kt
@@ -3,13 +3,12 @@ package com.example.parkover
 import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import androidx.cardview.widget.CardView
-import com.example.parkover.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.parkover.ar.ARFragment
+import com.example.parkover.databinding.ActivityMainBinding
+import com.example.parkover.databinding.CarBikeDailogeBinding
 import com.example.parkover.map.MapFragment
 import com.google.android.gms.maps.model.MarkerOptions
 import nl.joery.animatedbottombar.AnimatedBottomBar
@@ -25,9 +24,60 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        setContentView(R.layout.activity_main)
 
-        replaceFragment(MapFragment())
+        setupTab()
+        setupUI()
+
+    }
+
+    private fun setupUI() {
+        binding.carBike.setOnClickListener {
+            val dailogeBinding = CarBikeDailogeBinding.inflate(layoutInflater)
+            val myDailoge = Dialog(this)
+            myDailoge.setContentView(dailogeBinding.root)
+
+            myDailoge.setCancelable(true)
+            myDailoge.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDailoge.show()
+
+            dailogeBinding.carCard.setOnClickListener {
+                binding.carBike.setImageResource(R.drawable.ic_car_24)
+                myDailoge.dismiss()
+                //Animation
+                binding.carBike.animate().apply {
+                    duration = 1000
+                    rotationYBy(360f)
+                }.start()
+            }
+            dailogeBinding.bikeCard.setOnClickListener {
+                binding.carBike.setImageResource(R.drawable.ic_scooter)
+                myDailoge.dismiss()
+                //Animation
+                binding.carBike.animate().apply {
+                    duration = 1000
+                    rotationYBy(360f)
+                }.start()
+
+            }
+
+            //Animation start
+            binding.nav.setOnClickListener {
+                binding.nav.animate().apply {
+                    duration = 1000
+                    rotationYBy(360f)
+                }.withEndAction {
+                    binding.nav.animate().apply {
+                        duration = 1000
+                        rotationYBy(3600f)
+                    }.start()
+                }
+            }
+            //Animation End
+
+        }
+    }
+
+    private fun setupTab() {
         binding.bottomBar.setOnTabSelectListener(object :AnimatedBottomBar.OnTabSelectListener{
             override fun onTabSelected(
                 lastIndex: Int,
@@ -45,59 +95,8 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
-
-
-        val carDailoge = findViewById<ImageView>(R.id.car_bike)
-        carDailoge.setOnClickListener {
-            val dailogeBinding = layoutInflater.inflate(R.layout.car_bike_dailoge,null)
-            val myDailoge = Dialog(this)
-            myDailoge.setContentView(dailogeBinding)
-
-            myDailoge.setCancelable(true)
-            myDailoge.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-            myDailoge.show()
-
-            val carCard = dailogeBinding.findViewById<CardView>(R.id.car_card)
-            carCard.setOnClickListener {
-                carDailoge.setImageResource(R.drawable.ic_car_24)
-                myDailoge.dismiss()
-                //Animation
-                carDailoge.animate().apply {
-                    duration = 1000
-                    rotationYBy(360f)
-                }.start()
-            }
-            val bikeCard = dailogeBinding.findViewById<CardView>(R.id.bike_card)
-            bikeCard.setOnClickListener {
-                carDailoge.setImageResource(R.drawable.ic_scooter)
-                myDailoge.dismiss()
-                //Animation
-                carDailoge.animate().apply {
-                    duration = 1000
-                    rotationYBy(360f)
-                }.start()
-
-            }
-
-            //Animation start
-            val navImage = findViewById<ImageView>(R.id.nav)
-            navImage.setOnClickListener {
-                navImage.animate().apply {
-                    duration = 1000
-                    rotationYBy(360f)
-                }.withEndAction {
-                    navImage.animate().apply {
-                        duration = 1000
-                        rotationYBy(3600f)
-                    }.start()
-                }
-            }
-            //Animation End
-
-        }
-
-
-          }
+        replaceFragment(MapFragment())
+    }
 
 
     fun replaceFragment(fragment: Fragment) {

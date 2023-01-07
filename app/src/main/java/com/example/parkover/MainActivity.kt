b27a3e41da3080491afb99a475aa4491e94d4df0
Ankit <singhankit104555@gmail.com>
2023-01-08 03:58:48 +0530

    "Added DB"

--- app/src/main/java/com/example/parkover/MainActivity.kt
+++ app/src/main/java/com/example/parkover/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.parkover.add.AddActivity
@@ -12,24 +14,115 @@ import com.example.parkover.ar.ARFragment
 import com.example.parkover.databinding.ActivityMainBinding
 import com.example.parkover.databinding.CarBikeDailogeBinding
 import com.example.parkover.map.MapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import nl.joery.animatedbottombar.AnimatedBottomBar
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
 
+    var firebaseDatabase: FirebaseDatabase? = null
+    var databaseReference: DatabaseReference? = null
+    private var mAuth: FirebaseAuth? = null
+    var universalLat:Double = 23.1815
+    var universalLng:Double = 79.9864
+
     private lateinit var binding: ActivityMainBinding
     var lastMarker: MarkerOptions? = null
+    var latLng : LatLng= LatLng( universalLat, universalLng)
+    lateinit var coordinateModelArrayList: ArrayList<CoordinateModel>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        // initializing all our variables.
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        mAuth = FirebaseAuth.getInstance()
+        coordinateModelArrayList = ArrayList<CoordinateModel>()
+        // on below line we are getting database reference.
+        databaseReference = firebaseDatabase!!.getReference("Courses")
 
-        setupTab()
+
+//        setupTab()
         setupUI()
+//        insertData("yo","me","yyo")
+        insertData( 43.000000, -75.000000)
+        getCoordinates()
 
     }
+
+    private fun insertData(lat: Double, lng: Double) {
+        val coordinateModel = CoordinateModel( lat, lng)
+        val Uid = UUID.randomUUID().toString()
+        // on below line we are calling a add value event
+        // to pass data to firebase database.
+        databaseReference!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // on below line we are setting data in our firebase database.
+                databaseReference!!.child(Uid).setValue(coordinateModel)
+                // displaying a toast message.
+                Toast.makeText(applicationContext, "Coordinates Added..", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // displaying a failure message on below line.
+                Toast.makeText(applicationContext, "Fail to add Course..", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
+    }
+
+    private fun getCoordinates() {
+        // on below line clearing our list.
+//        courseRVModalArrayList.clear();
+        // on below line we are calling add child event listener method to read the data.
+        databaseReference!!.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                // on below line we are hiding our progress bar.
+//                loadingPB.setVisibility(View.GONE);
+                // adding snapshot to our array list
+                // on below line.
+                Toast.makeText(applicationContext, "Coordinates Loaded in map", Toast.LENGTH_SHORT).show()
+                snapshot.getValue(CoordinateModel::class.java)
+                    ?.let {
+                        coordinateModelArrayList.add(it)
+                        universalLat = it.getLat()
+                        universalLng = it.getLng()
+                        latLng = LatLng( universalLat, universalLng)
+                        setupTab()
+                    }
+
+                Log.i("MYTAG", "${coordinateModelArrayList.toString()} :'lng ' $universalLng,'lat ' $universalLat")
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                // this method is called when new child is added
+                // we are notifying our adapter and making progress bar
+                // visibility as gone.
+//                loadingPB.setVisibility(View.GONE);
+                Toast.makeText(applicationContext, "ChildChanged", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                // notifying our adapter when child is removed.
+//                loadingPB.setVisibility(View.GONE);
+                Toast.makeText(applicationContext, "ChildRemoved", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                // notifying our adapter when child is moved.
+//                loadingPB.setVisibility(View.GONE);
+                Toast.makeText(applicationContext, "ChildMoved", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
 
     private fun setupUI() {
         binding.add.setOnClickListener {

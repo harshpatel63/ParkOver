f852fc79dfb6e1a37f9584e0fafc9149744e8b11
Harsh <patelharsh0603@gmail.com>
2023-01-07 14:59:40 +0530

    Merge conflicts resolved

--- app/src/main/java/com/example/parkover/MainActivity.kt
+++ app/src/main/java/com/example/parkover/MainActivity.kt
@@ -4,21 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.parkover.databinding.ActivityMainBinding
 import androidx.fragment.app.Fragment
+import com.example.parkover.ar.ARFragment
+import com.example.parkover.map.MapFragment
 import nl.joery.animatedbottombar.AnimatedBottomBar
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var binding: ActivityMainBinding
-    private lateinit var bottomBar: AnimatedBottomBar
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        val view = binding.root
-//        setContentView(view)
-        setContentView(R.layout.activity_main)
-        bottomBar = findViewById(R.id.bottom_bar)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         replaceFragment(MapFragment())
-        bottomBar.setOnTabSelectListener(object :AnimatedBottomBar.OnTabSelectListener{
+        binding.bottomBar.setOnTabSelectListener(object :AnimatedBottomBar.OnTabSelectListener{
             override fun onTabSelected(
                 lastIndex: Int,
                 lastTab: AnimatedBottomBar.Tab?,
--- app/src/main/java/com/example/parkover/ar/ARFragment.kt
+++ app/src/main/java/com/example/parkover/ar/ARFragment.kt
@@ -1,10 +1,11 @@
-package com.example.parkover
+package com.example.parkover.ar
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.parkover.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +35,7 @@ class ARFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a_r, container, false)
+        return inflater.inflate(R.layout.fragment_ar, container, false)
     }
 
     companion object {
